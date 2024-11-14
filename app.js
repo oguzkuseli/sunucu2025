c40b5c4db88e010b0e0a39eb8057456ed6a48a34
oguzkuseli <oguz.kuseli@hotmail.com>
2024-11-14 15:11:09 +0300

    mysql bağlantısı kuruldu

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
+require("dotenv/config")
+const db_connect = require("./db/mysql_connect")
 const app = express()
-const port = 5000
+const port = process.env.PORT
+
 app.get("/",(req,res)=>{
   res.json({
     message:"Hoş geldiniz"
   })
 })
 
-app.listen(postMessage,()=>{
-  console.log("Sunucu 5000 portunda çalışıyor")
+app.listen(port,()=>{
+  console.log(`Sunucu ${port} portunda çalışıyor`)
 })
